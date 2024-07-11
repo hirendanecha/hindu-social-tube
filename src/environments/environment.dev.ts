552c4f2aa0dcad505d7bcfeb112dcfaca6290f47
ppa-opash <ppa.opash@gmail.com>
2024-07-11 16:48:31 +0530

    Apply recent change and add ad with link

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -16,7 +16,7 @@ export const environment = {
   backendUrl: backendUrl,
   loginUrl: loginUrl,
   apiUrl: `${backendUrl}api/v1/`,
-  domain: '.healing.tube',
+  domain: '.hindu.social',
   wasabiUrl: wasabiUrl,
   socketUrl: `${backendUrl}`,
   conferenceUrl: 'https://facetime.tube/',
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -15,7 +15,7 @@ export const environment = {
   backendUrl: backendUrl,
   loginUrl: loginUrl,
   apiUrl: `${backendUrl}api/v1/`,
-  domain: '.healing.tube',
+  domain: '.hindu.social',
   socketUrl: `${backendUrl}`,
   conferenceUrl: 'https://facetime.tube/',
   logoutUrl: logoutUrl
